2c4c00225656271480ceca93b6254e58b400667c
mcredou <matthieu@credou.bzh>
2015-09-26 17:12:35 +0200

    description added
    Added general description

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix Inversion function for COURSERA - R Programming - WEEK 3 - Assignment2
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
